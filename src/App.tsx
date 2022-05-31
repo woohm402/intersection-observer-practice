ad33d78714a1e61fcdcba9318c6f7b3bc5a5bbb8
woohm402 <woohm404@gmail.com>
2022-06-01 01:25:23 +0900

    기본 구조 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,42 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+interface Todo {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
 function App() {
-  return <div className="App"></div>;
+  const [page, setPage] = useState(1);
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const fetchTodo = async (p: number) => {
+    try {
+      const { data } = await axios.get<Todo>(
+        `https://jsonplaceholder.typicode.com/todos/${p}`
+      );
+      setTodos([...todos, data]);
+    } catch (err) {}
+  };
+
+  useEffect(() => {
+    fetchTodo(page);
+  }, [page]);
+
+  return (
+    <div className="App">
+      <ul>
+        {todos.map((item) => (
+          <li key={item.id}>
+            <h2>{item.title}</h2>
+            <p>{item.completed ? "done" : "not yet"}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
